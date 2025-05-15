2154c2cba23b9feedbd63fe503bcfd9a15fa48a8
Israel Glixinski <israel_glixinski@hotmail.com>
2025-05-15 16:41:14 -0300

    .

---
+++ primeiro_teste.py
@@ -0,0 +1,20 @@
+import os
+from pymongo import MongoClient
+
+mongo_url = os.getenv("MONGO_URL", "mongodb://172.32.1.73:27017/meubanco")
+client = MongoClient(mongo_url)
+db = client["meubanco"]
+
+# Exemplo de inserção
+db.coleta.insert_one({"mensagem": "Olá do robô!"})
+
+print("Documento inserido com sucesso!")
+
+consulta = db.coleta.find()
+
+for documento in consulta:
+    print(documento)
+
+
+
+
